6857f9af26e6f97ef0474d9fa607b40691e7a63b
Tobias Schubotz <tobias.schubotz@gmail.com>
2013-09-07 08:48:48 +0200

    added avatar url to account

--- access.rb
+++ access.rb
@@ -17,9 +17,7 @@ class Access
                             request_auth_info["expires_in"],
                             request_auth_info["refresh_token"])
     else
-      self.accounts << Account.new(user_info["id"],
-                            user_info["name"],
-                            user_info["phone"],
+      self.accounts << Account.new(user_info,
                             request_auth_info["access_token"],
                             request_auth_info["expires_in"],
                             request_auth_info["refresh_token"])
@@ -46,15 +44,17 @@ class Account
   attr_accessor :user_id
   attr_accessor :name
   attr_accessor :phone_number
+  attr_accessor :avatar_url
 
   attr_accessor :access_token
   attr_accessor :expires_in
   attr_accessor :refresh_token
 
-  def initialize(user_id, name, phone_number, access_token, expires_in, refresh_token)
-    self.user_id = user_id
-    self.name = name
-    self.phone_number = phone_number
+  def initialize(user_info, access_token, expires_in, refresh_token)
+    self.user_id = user_info["user_id"]
+    self.name = user_info["name"]
+    self.phone_number = user_info["phone"]
+    self.avatar_url = user_info["avatar_url"]
     self.access_token = access_token
     self.expires_in = expires_in
     self.refresh_token = refresh_token
