22cac64e66e16a0311b06f6d1703e4ed8b428873
Tobias Schubotz <tobias.schubotz@gmail.com>
2013-09-07 08:38:08 +0200

    refactored access to only create one account once

--- access.rb
+++ access.rb
@@ -7,23 +7,38 @@ class Access
   attr_accessor :accounts
 
   def initialize
-    @accounts = []
+    self.accounts = []
   end
 
   def add_account(request_auth_info)
     user_info = self.get_user_info(request_auth_info["access_token"])
-    accounts << Account.new(user_info["id"],
+    if account = self.get_account(user_info["id"])
+      account.update(request_auth_info["access_token"],
+                            request_auth_info["expires_in"],
+                            request_auth_info["refresh_token"])
+    else
+      self.accounts << Account.new(user_info["id"],
                             user_info["name"],
                             user_info["phone"],
                             request_auth_info["access_token"],
                             request_auth_info["expires_in"],
                             request_auth_info["refresh_token"])
+    end
   end
 
   def get_user_info(access_token)
     return HTTParty.get("https://api.box.com/2.0/users/me",
                             headers: {"Authorization" => "Bearer #{access_token}"}
                            )
+  end
+
+  def get_account(user_id)
+    self.accounts.each do |account|
+      if account.user_id == user_id
+        return account
+      end
+    end
+    return nil
   end
 end
 
@@ -37,11 +52,17 @@ class Account
   attr_accessor :refresh_token
 
   def initialize(user_id, name, phone_number, access_token, expires_in, refresh_token)
-    @user_id = user_id
-    @name = name
-    @phone_number = phone_number
-    @access_token = access_token
-    @expires_in = expires_in
-    @refresh_token = refresh_token
+    self.user_id = user_id
+    self.name = name
+    self.phone_number = phone_number
+    self.access_token = access_token
+    self.expires_in = expires_in
+    self.refresh_token = refresh_token
+  end
+
+  def update(access_token, expires_in, refresh_token)
+    self.access_token = access_token
+    self.expires_in = expires_in
+    self.refresh_token = refresh_token
   end
 end
